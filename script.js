bffa8d6dedcc44128ad0181c19b0fd24c9bc6619
ElfaLunar <luana.borini@escola.pr.gov.br>
2026-06-15 08:35:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let text = document.querySelector('.text');
 let spans = text.innerText.split('').map((letter, i) => {
   return `<span style="transition-delay:${i*40}ms; filter:hue-rotate(${i*30}deg)">${letter}
-  </span>
+  </span>`
   }).join('');
 
   text.innerHTML = spans;
